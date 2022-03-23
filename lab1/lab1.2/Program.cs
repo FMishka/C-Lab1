13ba85e91771fb772d4848a19435a069aea21620
FMishka <51850577+FMishka@users.noreply.github.com>
2022-03-23 19:30:59 +0300

    Add files via upload

---
+++ lab1/lab1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student() { Age = 20, Id = 123, GroupNumber = 22, Name = "Пётр"};
+            Student student2 = new Student() { Age = 22, Id = 345, GroupNumber = 42, Name = "Артём"};
+
+            student1.Add(student2);
+            student2.AddThree(student1, student2);
+
+            Console.WriteLine("Student1:");
+            Console.WriteLine($"Id = {student1.Id}, Age = {student1.Age}, Name = {student1.Name}, GroupNumber = {student1.GroupNumber}");
+            Console.WriteLine("Student2:");
+            Console.WriteLine($"Id = {student2.Id}, Age = {student2.Age}, Name = {student2.Name}, GroupNumber = {student2.GroupNumber}");
+        }
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Complex a = new Complex() { Real = 3, Imag = 0.5 };
+            Complex b = new Complex() { Real = 2, Imag = 0.8 };
+            a.Add(b);
+            Console.WriteLine($"a = {a.Real} + {a.Imag}i");
+            a.Substract(b);
+            Console.WriteLine($"a = {a.Real} + {a.Imag}i");
+            b.Multiply(a);
+            Console.WriteLine($"b = {b.Real} + {b.Imag}i");
+            b.Divide(a);
+            Console.WriteLine($"b = {b.Real} + {b.Imag}i");
+        }
+    }
+}
